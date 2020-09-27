5882780c94c4a9fcc59df90927c68934c7471669
Rohan Gaikwad <rohan.gaikwad@powerweave.com>
2020-09-28 02:47:38 +0530

    demo

--- dist/animate-io.js
+++ dist/animate-io.js
@@ -1,3 +1,9 @@
+/**
+ * https://github.com/rohangaikwad/animate-io
+ * Author: Rohan gaikwad
+ * Generated on Monday, September 28th 2020, 2:47:15 am
+ */
+
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
